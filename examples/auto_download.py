69c7c5f95774a25840f89f955a9a8f8fd1c03522
xuban ceccon <xuban.ceccon@gmail.com>
2026-01-03 21:57:59 +0100

    removed examples

--- examples/auto_download.py
+++
@@ -1,4 +0,0 @@
-from libreyolo import LIBREYOLO
-
-model = LIBREYOLO("libreyolo11n.pt",size="n")
-results = model(image="../media/test_image_1_creative_commons.jpg", save=True)
--- examples/run_rtdtr_inference.py
+++
@@ -1,7 +0,0 @@
-from libreyolo import LIBREYOLO
-
-model = LIBREYOLO("librertdetrs.pth", size="s")
-
-result = model("/Users/xuban.ceccon/Documents/GitHub/libreyolo/media/parkour.jpg", save=True, output_path="/Users/xuban.ceccon/Documents/GitHub/libreyolo/examples/testazo")
-
-
